9b69295480226a1b54e25cdd344401d9a3cb24fb
takahiro.kawaguchi <takahiro0402@icloud.com>
2020-11-12 07:19:17 +0000

    tasuku-tuika2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks=Task.all
@@ -45,14 +45,14 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
 
   private
   
-  def set_message
+  def set_task
     @task = Task.find(params[:id])
   end
 
   def task_params
     params.require(:task).permit(:content,:status)
   end
+end
